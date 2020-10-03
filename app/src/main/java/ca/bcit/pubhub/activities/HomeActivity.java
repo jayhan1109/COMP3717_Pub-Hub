d69bb2d8c7e6df0d9de9cb1b5abbfca57de9e0f8
jayhan1109 <jayhan1109@gmail.com>
2020-10-04 02:03:08 +0900

    Login Update

---
+++ app/src/main/java/ca/bcit/pubhub/activities/HomeActivity.java
@@ -0,0 +1,50 @@
+package ca.bcit.pubhub.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import ca.bcit.pubhub.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        checkCurrentUser();
+    }
+
+    public void checkCurrentUser() {
+        // [START check_current_user]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+        } else {
+            // No user is signed in
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        // [END check_current_user]
+    }
+
+    public void logoutClick(View view) {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        checkCurrentUser();
+                    }
+                });
+        // [END auth_fui_signout]
+    }
+}
