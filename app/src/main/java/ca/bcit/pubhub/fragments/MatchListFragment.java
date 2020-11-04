a93c6062efcf22fc7ea79e07e5d15f7af6727f54
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2020-11-05 04:28:22 +0900

    done

--- app/src/main/java/ca/bcit/pubhub/fragments/MatchListFragment.java
+++ app/src/main/java/ca/bcit/pubhub/fragments/MatchListFragment.java
@@ -1,30 +1,51 @@
 package ca.bcit.pubhub.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Date;
 
+import ca.bcit.pubhub.activities.ChatActivity;
+import ca.bcit.pubhub.activities.History;
 import ca.bcit.pubhub.adapters.MatchViewAdapter;
 import ca.bcit.pubhub.items.MatchViewItem;
 import ca.bcit.pubhub.utils.Match;
 import ca.bcit.pubhub.utils.MatchModel;
 
 public class MatchListFragment extends ListFragment {
-
     MatchViewAdapter adapter;
+    DatabaseReference myRef;
+    String userId;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         adapter = new MatchViewAdapter();
         setListAdapter(adapter);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        userId = user.getUid();
+        myRef = database.getReference("history/"+ userId);
+
 
         for (
                 MatchModel m : Match.soccer_epl) {
@@ -51,5 +72,39 @@ public class MatchListFragment extends ListFragment {
 
     public void addItem(int c, int m, String t1, String t2, Date d) {
         adapter.addItem(c, m, t1, t2, d);
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        Intent intent = new Intent(getActivity(), ChatActivity.class);
+        final MatchViewItem item = adapter.getMatchViewItem(position);
+        final int matchId = item.get_matchId();
+        History history = new History();
+        history.setMatchID(item.get_matchId());
+        history.setHistoryTime(ServerValue.TIMESTAMP);
+        history.setMatchTime(item.get_date().toString());
+        history.setMatchName(item.get_team1() + " vs " + item.get_team2());
+        System.out.println(item.get_team1() + item.get_team2());
+        intent.putExtra("chatId", Integer.toString(matchId));
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    History h = snapshot.getValue(History.class);
+                    if(h.getMatchID() == matchId){
+//                                String key = snapshot.getKey();
+                        DatabaseReference newRef = snapshot.getRef();
+                        newRef.removeValue();
+                        return;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        myRef.push().setValue(history);
+        startActivity(intent);
     }
 }
